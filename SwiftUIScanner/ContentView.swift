72397ac0a357b3922b79794d4942680b84e423a4
Tim Owings <owingst@gmail.com>
2019-11-03 14:00:39 -0600

    Initial Commit

---
+++ SwiftUIScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIScanner
+//
+//  Created by Tim Owings on 11/3/19.
+//  Copyright © 2019 Tim Owings. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
